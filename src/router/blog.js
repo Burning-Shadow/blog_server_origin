542e659cd35bcb1c277689458b2b8d5d7ca1d9f8
Siir <2485999859@qq.com>
2020-02-15 20:56:32 +0800

    redis嵌入

--- src/router/blog.js
+++ src/router/blog.js
@@ -7,6 +7,13 @@ const {
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
 
+// 登陆验证
+const loginCheck = req => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel("尚未登陆"));
+  }
+};
+
 const handleBlogRouter = (req, res) => {
   const method = req.method;
   const id = req.query.id;
@@ -28,13 +35,26 @@ const handleBlogRouter = (req, res) => {
     });
   } else if (method === "POST" && req.path === "/api/blog/new") {
     // TODO: 登陆后方可新建博客。此时 author 从后台直接取即可，目前使用假数据，日后更改
-    req.body.author = "testAuthor";
+
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
+    req.body.author = req.session.username;
     const result = newBlog(req.body);
 
     return result.then(data => {
       return new SuccessModel(data);
     });
   } else if (method === "POST" && req.path === "/api/blog/update") {
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
     const result = updateBlog(id, req.body);
 
     return result.then(val => {
@@ -45,9 +65,14 @@ const handleBlogRouter = (req, res) => {
       }
     });
   } else if (method === "POST" && req.path === "/api/blog/delete") {
-    // TODO: 用户名暂且使用虚假数据，删除时只能删除自己的文章
-    const author = "falseAuthor";
-    const result = delBlog(id,author);
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
+    const author = req.session.username;
+    const result = delBlog(id, author);
 
     return result.then(val => {
       if (val) {
